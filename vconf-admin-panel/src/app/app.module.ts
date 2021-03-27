e9668ec93bb4065cc7f5e5b38218aaca5fd7a834
akash-pateliya <akashpateliya00@gmail.com>
2021-03-27 13:41:18 +0530

    add basics components and services file

--- vconf-admin-panel/src/app/app.module.ts
+++ vconf-admin-panel/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { SigninComponent } from './admin/signin/signin.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { OrdersListComponent } from './orders/orders-list/orders-list.component';
+import { VehicleAddComponent } from './vehicles/vehicle-add/vehicle-add.component';
+import { VehiclesListComponent } from './vehicles/vehicles-list/vehicles-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    UsersListComponent,
+    OrdersListComponent,
+    VehicleAddComponent,
+    VehiclesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule
   ],
   providers: [],
   bootstrap: [AppComponent]
