d684a99c3a5af31413946da1c8d070875a23cab5
akash-pateliya <akashpateliya00@gmail.com>
2021-03-27 13:48:16 +0530

    add basic routing

--- vconf-admin-panel/src/app/app-routing.module.ts
+++ vconf-admin-panel/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './admin/signin/signin.component';
+import { OrdersListComponent } from './orders/orders-list/orders-list.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { VehicleAddComponent } from './vehicles/vehicle-add/vehicle-add.component';
+import { VehiclesListComponent } from './vehicles/vehicles-list/vehicles-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'orders-list', component: OrdersListComponent },
+  { path: 'users-list', component: UsersListComponent },
+  { path: 'vehicles-list', component: VehiclesListComponent },
+  { path: 'vehicle-add', component: VehicleAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
